2b858a149a9182341c2e58d47c967f4ea52f140e
linshun <linshun@cocos2d-x.org>
2013-11-05 00:14:16 +0800

    Add ogg format audio.

--- Cocos2d-html5-v2.2/HalloweenDefence/src/resource.js
+++ Cocos2d-html5-v2.2/HalloweenDefence/src/resource.js
@@ -9,9 +9,14 @@ var s_HighBullet = "res/HighShell.png";
 var s_AttackRange = "res/AttackRange.png";
 
 var s_AttackEffect_mp3 = "res/Music/AttackEffect.mp3";
+var s_AttackEffect_ogg = "res/Music/AttackEffect.ogg";
 var s_AttackHighEffect_mp3 = "res/Music/AttackHighEffect.mp3";
+var s_AttackHighEffect_ogg = "res/Music/AttackHighEffect.ogg";
 var s_MonsterDie_mp3 = "res/Music/MonsterDie.mp3";
+var s_MonsterDie_ogg = "res/Music/MonsterDie.ogg";
 var s_MonsterAcross_mp3 = "res/Music/MonsterAcross.mp3";
+var s_MonsterAcross_ogg = "res/Music/MonsterAcross.ogg";
+
 
 var s_Monster = [
 	"res/Monster-1.png",
@@ -31,7 +36,6 @@ var s_TowerBall = [
 
 var g_resources = [
     //image
-	{src:s_DtMapsTmx},
 	{src:s_DtMapsImage},
 	{src:s_ToolsBar},
 
@@ -51,20 +55,26 @@ var g_resources = [
 	{src:s_TowerBall[0]},
 	{src:s_TowerBall[1]},
 
+    //effect
 	{src:s_AttackEffect_mp3},
-	{src:s_AttackHighEffect_mp3},
+    {src:s_AttackEffect_ogg},
+    {src:s_AttackHighEffect_mp3},
+    {src:s_AttackHighEffect_ogg},
 	{src:s_MonsterDie_mp3},
-	{src:s_MonsterAcross_mp3}
+    {src:s_MonsterDie_ogg},
+	{src:s_MonsterAcross_mp3},
+    {src:s_MonsterAcross_ogg},
 
     //plist
 
     //fnt
 
     //tmx
+    {src:s_DtMapsTmx}
 
     //bgm
 
-    //effect
+
 ];
 
 
